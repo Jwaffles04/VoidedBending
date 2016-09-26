fbda5d14d2fcb3836b35768a1e8b02e652ecedc1
pammykool <pammykool@gmail.com>
2016-09-26 19:00:10 -0400

    AirAura.java

---
+++ src/air/AirAura.java
@@ -0,0 +1,40 @@
+package air;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+import main.BaseAbility;
+
+public class AirAura extends BaseAbility {
+
+	  private Player player;
+	  private int t;
+
+	  public AirAura(Player player) {
+	    this.player = player;
+	    add(this);
+	  }
+
+	  @Override
+	  public boolean progress() {
+	    if (player == null || !player.isOnline() || player.isDead() || t > 5 * 20) {
+	      return false;
+	    }
+	    //circle effect
+	    //thingie effect
+	    for (Entity entity : player.getNearbyEntities(5, 5, 5)) {
+	      if (entity instanceof LivingEntity && !entity.equals(player)) {
+	        // Pushes Entity.
+	        entity.setVelocity(null);
+	      }
+	    }
+		return false;
+	  }
+
+	  @Override
+	  public void stop() {
+	    // we dont have a cooldown.
+	  }
+
+	}
